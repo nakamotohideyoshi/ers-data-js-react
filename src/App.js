cbc6e73eae4dac4cfa58c50ab671ae7fd01ed911
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-02-21 06:11:16 -0600

    HeaderNavigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import HeaderNavigation from './containers/navbar';
 import JumbotronContainer from './containers/jumbotron';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <HeaderNavigation />
         <JumbotronContainer title="ARMS Farm Structure and Finance" />
       </div>
     );
