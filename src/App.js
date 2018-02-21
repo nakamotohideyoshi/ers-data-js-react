c8f3daf2f50c924aff15c233ba43e5d798098a44
Mario <mariogal.jr@gmail.com>
2018-02-20 21:38:52 -0600

    App.js with Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
-import logo from './logo.svg';
-import './App.css';
+import Jumbotron from './containers/jumbotron';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button>Click me!</Button>
+        <Jumbotron title="ARMS Farm Structure and Finance" />
       </div>
     );
   }
