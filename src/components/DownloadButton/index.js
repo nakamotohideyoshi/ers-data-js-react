f1a7870e47d5353c3fdef9ffa7cec30fe0d0b31c
Mario <mariogal.jr@gmail.com>
2018-02-21 22:51:24 -0600

    Download Button Component

---
+++ src/components/DownloadButton/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
+import DownloadImg from '../../images/download.png'
+import './style.css';
+
+export default class MainContainer extends React.Component {
+  render() {
+    return (
+          <div className="btn-second">
+            <DropdownButton
+              bsStyle="default"
+              bsSize="medium"
+              pullRight
+              title={<div>
+              <img src={DownloadImg} alt="" /> Download
+            </div>}
+              noCaret
+              id="dropdown-no-caret"
+            >
+              <MenuItem eventKey="1">PDF</MenuItem>
+              <MenuItem eventKey="2">PNG</MenuItem>
+              <MenuItem eventKey="3">EMBED</MenuItem>
+              <MenuItem eventKey="4">API</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="5">Chart (CSV)</MenuItem>
+              <MenuItem eventKey="6">Table (CSV)</MenuItem>
+            </DropdownButton>
+          </div>
+    )
+  }
+}
