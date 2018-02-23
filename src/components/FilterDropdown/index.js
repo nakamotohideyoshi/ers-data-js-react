435ff9b1c99bdbc82a407bc61d49aca8ccac0cb7
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-02-23 11:48:43 -0600

    Filter dropdown component

---
+++ src/components/FilterDropdown/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, DropdownButton } from 'react-bootstrap';
+import Checkbox from '../Checkbox';
+import './style.css';
+
+const FilterDropdown = ({ onSelectYear, yearsInfo }) => (
+  <Row className="filterDropdownContainer">
+    <Col md={6} sm={6} xs={12}>
+      <Col md={7} sm={6} xs={12} mdOffset={5} smOffset={6}>
+        <div className="top-title right-title">VIEW MULTIPLE YEARS</div>
+        <DropdownButton
+          bsStyle="default"
+          title="Years"
+          id="dropdown-years"
+          className="download-menu"
+        >
+        {
+          yearsInfo.map((infoObj, index) => {
+            return <Checkbox title={infoObj.year} checked={infoObj.checked} onCheck={() => onSelectYear(index)} key={index} />
+          })
+        }
+        </DropdownButton>
+      </Col>
+    </Col>
+    <Col md={6} sm={6} xs={12}>
+        <Col md={1} sm={1} xs={1}>
+        </Col>
+        <Col md={7} sm={6} xs={11}>
+          <div className="top-title">FILTERED BY REGION</div>
+          <DropdownButton
+            bsStyle="default"
+            title="Region"
+            id="dropdown-region"
+            className="download-menu"
+          >
+            <Checkbox title="California" />        	                 	
+          </DropdownButton>
+        </Col>
+    </Col>    
+  </Row>
+  
+);
+
+FilterDropdown.propTypes = {
+  onSelectYear: PropTypes.func,
+  yearsInfo: PropTypes.array,
+};
+
+export default FilterDropdown;
