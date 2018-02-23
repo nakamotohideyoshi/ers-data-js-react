05203155e35c694de19c54bb05982b2d8da5232c
Mario <mariogal.jr@gmail.com>
2018-02-23 03:35:35 -0600

    Rename as Uppercase

---
+++ src/containers/Sidebar/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Reset from '../../images/reset.png'
+import './style.css';
+
+const Sidebar = () => (
+  <Col sm={3} md={3} xs={12} className="right-padding">
+    <Row className="dropdown_up dropdown_dd">
+      <button className="btn-dark btn_dd" type="button" >
+        Tailored Reports
+        <span className="caret_down">
+          <i className="fa fa-caret-down"></i>
+        </span>
+      </button>
+    </Row>
+    <Row>
+      <a className="pull-right reset">
+        <img src={Reset} alt="" />Reset
+      </a>
+    </Row>
+    <Row className="dropdown_up dropdown_dd">
+      <button className="btn-light btn_dd_1 collapsed">
+        <span className="top_heading">report</span>
+        <br />
+        Farm Business Income Statement 
+        <span className="caret_down_sec">
+          <i className="fa fa-caret-down"></i>
+        </span>
+      </button>
+    </Row>
+    <Row className="dropdown_up dropdown_dd">
+      <button className="btn-light btn_dd_1 collapsed">
+        <span className="top_heading">subject</span>
+        <br />
+        All Farms
+        <span className="caret_down_sec">
+          <i className="fa fa-caret-down"></i>
+        </span>
+      </button>
+    </Row>
+    <Row className="dropdown_up dropdown_dd">
+      <button className="btn-light btn_dd_1 collapsed">
+        <span className="top_heading">filter by</span>
+        <br />
+        Operator Age
+        <span className="caret_down_sec">
+          <i className="fa fa-caret-down"></i>
+        </span>
+      </button>
+    </Row>
+    <Row className="dropdown_up dropdown_dd">
+      <button className="btn-light btn_dd_1 collapsed">
+        <span className="top_heading">objector age</span>
+        <br />
+        Objectors 34 years or younger
+        <span className="caret_down_sec">
+          <i className="fa fa-caret-down"></i>
+        </span>
+      </button>
+    </Row>
+  </Col>
+);
+
+export default Sidebar;
