67271a8348858da23f1637548ad1d4bb89a2afd0
Mario <mariogal.jr@gmail.com>
2018-02-25 13:29:02 -0600

    LHS sidebar

---
+++ src/components/SidebarItem/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SidebarDropdown from '../SidebarDropdown'
+import { SlideDown } from 'react-slidedown'
+import Reset from '../../images/reset.png'
+
+const SidebarItem = ({ titles, selectedIndex, isOpened, isCategory, toggleCategoryOptions, updateFilter }) => (
+  <div className="dropdown_second">
+    <SidebarDropdown title={titles[selectedIndex]} isOpened={isOpened} isCategory={isCategory} onToggle={() => toggleCategoryOptions()} />
+    <SlideDown className='my-dropdown-slidedown'>
+      {
+        isOpened && (
+          <ul className="dropdown-menu_2 collapse in">
+          {
+            titles.map((val, index) => {
+              return (
+                <li onClick={()=>updateFilter(index)} className={`${selectedIndex === index ? `active`:``}`}>
+                  <a>
+                  {
+                    selectedIndex === index && (
+                      <div><i className="fa fa-check"></i>{titles[index]}</div>
+                    ) || (
+                      <div className="right-padding-option">{titles[index]}</div>
+                    )
+                  }
+                  
+                  </a>
+                </li>
+              )
+            })
+          }
+          </ul>              
+        )
+      }
+    </SlideDown> 
+    {
+      isCategory && (
+        <div>
+          <a className="pull-right reset">
+            <img src={Reset} alt="" />Reset
+          </a>
+        </div>
+      )
+    }   
+  </div>
+);
+
+SidebarItem.propTypes = {
+  titles: PropTypes.array,
+  siebarItemIndex: PropTypes.number,
+  isOpened: PropTypes.bool,   
+  isCategory: PropTypes.bool,
+  toggleCategoryOptions: PropTypes.func,
+  updateFilter: PropTypes.func  
+};
+
+SidebarItem.defaultProps = {
+  titles: [],
+  siebarItemIndex: 0,  
+  isOpened: false,  
+  isCategory: false 
+};
+
+export default SidebarItem;
--- src/containers/Sidebar/index.js
+++ src/containers/Sidebar/index.js
@@ -1,47 +1,61 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
-import {SlideDown} from 'react-slidedown'
 import 'react-slidedown/lib/slidedown.css'
-import Reset from '../../images/reset.png'
-import SidebarDropdown from '../../components/SidebarDropdown'
+import SidebarItem from '../../components/SidebarItem'
 import './style.css';
 
+const categoryTitles = [
+  ['Tailored Reports', 'ARMS Data Analysis'],
+  ['Farm Business Balance Sheet', 'Farm Business Income Statement', 'Farm Business Financila Ratiost', 'Strctural Caracteristics', 'Farm Business Debt Repayment Capacity', 'Strctural Caracteristics',  'Farm Business Debt Repayment Capacity',  'Govenerment Payments',  'Operator Household Income',  'Operator Household Balance Sheet'],
+  ['All Farms', 'Farm Businesses', 'Farm Operator Households'], 
+  ['All Farms', 'Collapsed Farm Typology', 'Economic Class', 'Farm Typology', 'Operator Age', 'Farm Resource REgion', 'Production Specialty'],
+  ['All Survey states', 'Arkansas', 'California', 'Florida', 'Georgia', 'Illinois', 'Indiana', 'Iowa',  'Kansas', 'Minnesota', 'Missouri', 'Nebraska', 'North Carolina', 'Texas', 'Washington', 'Wisconsin'],
+  ['All Farms', 'Collapsed Farm Typology', 'Economic Class', 'Farm Typology', 'Operator Age', 'Farm Resource REgion', 'Production Specialty'],
+  ['2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998', '1997', '1996']
+]
+const sidebarItems = [
+  {isOpened: false, selectedIndex: 0, isCategory: true, headingTitle: "", },
+  {isOpened: false, selectedIndex: 0, isCategory: false, headingTitle: "Report"},
+  {isOpened: false, selectedIndex: 0, isCategory: false, headingTitle: "Subject"},
+  {isOpened: false, selectedIndex: 0, isCategory: false, headingTitle: "Row group"},
+  {isOpened: false, selectedIndex: 0, isCategory: false, headingTitle: "Filter by US or State"},
+  {isOpened: false, selectedIndex: 0, isCategory: false, headingTitle: "Sub group"},  
+  {isOpened: false, selectedIndex: 0, isCategory: false, headingTitle: "From year"}  
+]
 export default class Sidebar extends React.Component {
   state = {
     isCategoryOpened: false,
-    filterCategory: 0
+    sidebarItems: sidebarItems
   }
-  toggleCategoryOptions = () => {
-    this.setState({ isCategoryOpened: !this.state.isCategoryOpened })
+  toggleCategoryOptions = (selectedItemIndex) => {
+    const { sidebarItems } =this.state
+    sidebarItems[selectedItemIndex].isOpened = !sidebarItems[selectedItemIndex].isOpened
+    this.setState({ sidebarItems })
   }
-  updateCategory = (val) => {
-    this.setState({ filterCategory: val })
-    this.setState({ isCategoryOpened: false })
+  updateFilter = (sidebarItemIndex, selectedIndex) => {
+    const { sidebarItems } =this.state
+    sidebarItems[sidebarItemIndex].selectedIndex = selectedIndex
+    this.setState({ sidebarItems })
+    this.toggleCategoryOptions(sidebarItemIndex)
   }
   render() {
-    const { isCategoryOpened, filterCategory} = this.state
     return (
     <Col sm={3} md={3} xs={12} className="sidebar-container">
-        <SidebarDropdown title={`${filterCategory ? `ARMS Data Analysis`:`Tailored Reports`}`} isCategory={true} onToggle={this.toggleCategoryOptions} />
-        <SlideDown className={'my-dropdown-slidedown'}>
-          {
-            isCategoryOpened && (
-              <ul className="dropdown-menu_2 collapse in">
-                <li onClick={()=>this.updateCategory(0)} className={`${filterCategory === 0 ? `active`:``}`}><a><i class="fa fa-check"></i> Tailored Reports</a></li>
-                <li onClick={()=>this.updateCategory(1)} className={`${filterCategory ? `active`:``}`}><a><i class="fa fa-check"></i>ARMS Data Analysis</a></li>  
-              </ul>
-            )
-          }
-        </SlideDown>
-        <div>
-          <a className="pull-right reset">
-            <img src={Reset} alt="" />Reset
-          </a>
-        </div>
-        <SidebarDropdown title="Farm Business Income Statement" onToggle={this.toggleView} />
-        <SidebarDropdown title="All Farms" onToggle={this.toggleView} />
-        <SidebarDropdown title="All Farms" headingTitle="filter by" onToggle={this.toggleView} />
-        <SidebarDropdown title="Objectors 34 years or younger" headingTitle="objector age" onToggle={this.toggleView} />
+      {
+        sidebarItems.map((val, i) => {
+          return (
+            <SidebarItem 
+              headingTitle={val.headingTitle}
+              titles={categoryTitles[i]}
+              selectedIndex={sidebarItems[i].selectedIndex}
+              isOpened={sidebarItems[i].isOpened}
+              isCategory={sidebarItems[i].isCategory} 
+              toggleCategoryOptions={() => this.toggleCategoryOptions(i)}
+              updateFilter={(index) => this.updateFilter(i, index)}          
+            />
+          )
+        })
+      }
       </Col>
     )
   }
