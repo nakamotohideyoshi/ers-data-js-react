85e3bd537cbd4689c48a79e5c26ba64934354a08
Mario <mariogal.jr@gmail.com>
2018-02-24 20:54:19 -0600

    Sidebar container

--- src/containers/Sidebar/index.js
+++ src/containers/Sidebar/index.js
@@ -1,64 +1,49 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import {SlideDown} from 'react-slidedown'
+import 'react-slidedown/lib/slidedown.css'
 import Reset from '../../images/reset.png'
+import SidebarDropdown from '../../components/SidebarDropdown'
 import './style.css';
 
-const Sidebar = () => (
-  <Col sm={3} md={3} xs={12} className="right-padding">
-    <Row className="dropdown_up dropdown_dd">
-      <button className="btn-dark btn_dd" type="button" >
-        Tailored Reports
-        <span className="caret_down">
-          <i className="fa fa-caret-down"></i>
-        </span>
-      </button>
-    </Row>
-    <Row>
-      <a className="pull-right reset">
-        <img src={Reset} alt="" />Reset
-      </a>
-    </Row>
-    <Row className="dropdown_up dropdown_dd">
-      <button className="btn-light btn_dd_1 collapsed">
-        <span className="top_heading">report</span>
-        <br />
-        Farm Business Income Statement 
-        <span className="caret_down_sec">
-          <i className="fa fa-caret-down"></i>
-        </span>
-      </button>
-    </Row>
-    <Row className="dropdown_up dropdown_dd">
-      <button className="btn-light btn_dd_1 collapsed">
-        <span className="top_heading">subject</span>
-        <br />
-        All Farms
-        <span className="caret_down_sec">
-          <i className="fa fa-caret-down"></i>
-        </span>
-      </button>
-    </Row>
-    <Row className="dropdown_up dropdown_dd">
-      <button className="btn-light btn_dd_1 collapsed">
-        <span className="top_heading">filter by</span>
-        <br />
-        Operator Age
-        <span className="caret_down_sec">
-          <i className="fa fa-caret-down"></i>
-        </span>
-      </button>
-    </Row>
-    <Row className="dropdown_up dropdown_dd">
-      <button className="btn-light btn_dd_1 collapsed">
-        <span className="top_heading">objector age</span>
-        <br />
-        Objectors 34 years or younger
-        <span className="caret_down_sec">
-          <i className="fa fa-caret-down"></i>
-        </span>
-      </button>
-    </Row>
-  </Col>
-);
+export default class Sidebar extends React.Component {
+  state = {
+    isCategoryOpened: false,
+    filterCategory: 0
+  }
+  toggleCategoryOptions = () => {
+    this.setState({ isCategoryOpened: !this.state.isCategoryOpened })
+  }
+  updateCategory = (val) => {
+    this.setState({ filterCategory: val })
+    this.setState({ isCategoryOpened: false })
+  }
+  render() {
+    const { isCategoryOpened, filterCategory} = this.state
+    return (
+    <Col sm={3} md={3} xs={12} className="sidebar-container">
+        <SidebarDropdown title={`${filterCategory ? `ARMS Data Analysis`:`Tailored Reports`}`} isCategory={true} onToggle={this.toggleCategoryOptions} />
+        <SlideDown className={'my-dropdown-slidedown'}>
+          {
+            isCategoryOpened && (
+              <ul className="dropdown-menu_2 collapse in">
+                <li onClick={()=>this.updateCategory(0)} className={`${filterCategory === 0 ? `active`:``}`}><a><i class="fa fa-check"></i> Tailored Reports</a></li>
+                <li onClick={()=>this.updateCategory(1)} className={`${filterCategory ? `active`:``}`}><a><i class="fa fa-check"></i>ARMS Data Analysis</a></li>  
+              </ul>
+            )
+          }
+        </SlideDown>
+        <div>
+          <a className="pull-right reset">
+            <img src={Reset} alt="" />Reset
+          </a>
+        </div>
+        <SidebarDropdown title="Farm Business Income Statement" onToggle={this.toggleView} />
+        <SidebarDropdown title="All Farms" onToggle={this.toggleView} />
+        <SidebarDropdown title="All Farms" headingTitle="filter by" onToggle={this.toggleView} />
+        <SidebarDropdown title="Objectors 34 years or younger" headingTitle="objector age" onToggle={this.toggleView} />
+      </Col>
+    )
+  }
+}
 
-export default Sidebar;
