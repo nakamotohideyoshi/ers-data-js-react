072c4f72bb2403f13f0b8ccae900da14b83b13d8
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-02-26 08:12:06 -0800

    implement years filter

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import { Grid } from 'react-bootstrap';
 import Sidebar from '../Sidebar';
 import MainContainer from '../MainContainer';
 
-export default class Layout extends React.Component {
+
+class Layout extends React.Component {
   render() {
-    return (
-      <Grid>
-        <Sidebar />
-        <MainContainer />
-      </Grid>
-    )
+    if(this.props.data.arms_year) {
+      return (
+        <Grid>
+          <Sidebar />
+          <MainContainer years={this.props.data.arms_year} />
+        </Grid>
+      )
+    } else {
+      return(
+        <p>Loading...</p>
+      )
+    }
+    
   }
 }
+
+const years_list = gql`{ 
+  arms_year
+ }`;
+
+export default graphql(years_list)(Layout);
--- src/containers/MainContainer/index.js
+++ src/containers/MainContainer/index.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import DownloadButton from '../../components/DownloadButton';
 import SheetDataChart from '../../components/SheetDataChart';
 import FilterDropdown from '../../components/FilterDropdown';
 import Footnote from '../Footnote';
 import './style.css';
 
-const years = ['2007', '2008', '2009', '2010', '2011', '2012', '2013',  
-               '2014', '2015', '2016', '2017', '2018', '2019', '2020']
+class MainContainer extends React.Component {  
+  constructor(props) {
+    super(props);
 
-export default class MainContainer extends React.Component {
-  componentWillMount() {
+    this.state = {
+      yearsInfo: [],
+    }
+  }
+  componentWillReceiveProps(props) { 
     let yearsInfo = []
-    years.forEach(yearNumber => {
-      const infoObj = {}
-      infoObj.year = yearNumber
-      infoObj.checked = false
-      infoObj.grossCashIncome = Math.floor((Math.random() * 250) + 1);
-      infoObj.totalCashExpense = Math.floor((Math.random() * 250) + 1);
-      infoObj.variableExpense = Math.floor((Math.random() * 250) + 1);
-      infoObj.netFarmIncome = Math.floor((Math.random() * 250) + 1);
-      yearsInfo.push(infoObj)
-    });
-    this.setState({ yearsInfo })
+    if (props.years) {
+      props.years.forEach(yearN => {
+        const infoObj = {}
+          infoObj.year = yearN
+          infoObj.checked = false
+          yearsInfo.push(infoObj)
+      })    
+    }
+   
+    if(props.data.arms_surveydata) {
+      props.data.arms_surveydata.forEach(surveyData => {
+        yearsInfo.forEach((info, index) => {
+          if (info.year == surveyData.year) {
+            if (surveyData.topic_abb == "igcfi") {
+              yearsInfo[index].grossCashIncome = surveyData.estimate
+            } else if (surveyData.topic_abb == "etot") {
+              yearsInfo[index].totalCashExpense = surveyData.estimate
+            } else if (surveyData.topic_abb == "evtot") {
+              yearsInfo[index].variableExpense = surveyData.estimate
+            } else if (surveyData.topic_abb == "infi") {
+              yearsInfo[index].netFarmIncome = surveyData.estimate
+            }
+            return true
+          }
+        })
+      });
+      this.setState({ yearsInfo })
+    }
   }
   onSelectYear = (index) => {
     let { yearsInfo } = this.state
@@ -46,3 +69,35 @@ export default class MainContainer extends React.Component {
     )
   }
 }
+const years_list = gql`
+  query Getquery($years: [Int]) {
+    arms_surveydata(
+      year: $years,
+      subject_num: 1,
+      series: "farm",
+      series_element: 0,
+      series2: "farm",
+      series2_element: 0,
+      report_num: [2],
+      state_id: "00",
+      topic_abb: ["igcfi", "etot", "evtot", "infi"]
+    )
+    {
+      year
+      rse
+      estimate
+      topic_abb
+      topic_dim {
+        level
+        seq
+        abb
+        header
+      }
+      
+    }
+  }
+`
+const options = ({ years, }) => ({variables: { years, }, })
+
+export default graphql(years_list, { options })(MainContainer);
+
