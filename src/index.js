8c656de61a9a32e87d2b608c0c772aef09632096
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-02-26 08:10:33 -0800

    install react-apollo GraphQL client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const client = new ApolloClient({
+  link: createHttpLink({ uri: 'http://query.erspilot.net/graphql?dev=twollegPBxv1pV82fws9'}),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render
+( 
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
