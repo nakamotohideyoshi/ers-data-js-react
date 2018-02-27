e72764a45a1da2033f85327dc87d4b3992bd941f
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-02-27 12:37:41 -0800

    years filter

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,31 +1,48 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
 import { Grid } from 'react-bootstrap';
 import Sidebar from '../Sidebar';
 import MainContainer from '../MainContainer';
+const filters = [
+  [],
+  [1, 2, 3, 4, 5, 6, 7, 8],
+  [1, 2, 3],
+  ['farm', 'grp', 'sal', 'stypll', 'age', 'reg', 'spec'],
+  []
+]
 
-
-class Layout extends React.Component {
-  render() {
-    if(this.props.data.arms_year) {
-      return (
-        <Grid>
-          <Sidebar />
-          <MainContainer years={this.props.data.arms_year} />
-        </Grid>
-      )
-    } else {
-      return(
-        <p>Loading...</p>
-      )
-    }
+export default class Layout extends React.Component {
+ 
+  componentWillMount() {
+    const subject_num = [1];
+    const series = ['farm'];
+    const report_num = [2];
+    const selected_years = [];
+    const {years} = this.props
+    this.setState({ subject_num, series, report_num, selected_years, years })
     
+  }
+  onSelectYear1 = (selectedyears) => {
+    let {selected_years} = this.state
+    selected_years = selectedyears    
+    this.setState({ selected_years })
+  }
+  render() {
+    const { subject_num, series, report_num, selected_years, years} = this.state
+    console.log(this.state)    
+    return (
+      <Grid>
+        <Sidebar
+        />
+        <MainContainer 
+          years={years}
+          subjectnum = {subject_num}
+          series1 = {series}
+          reportnum = {report_num}
+          selectedyears = {selected_years}
+          onSelectYear1 = {this.onSelectYear1}
+        />
+      </Grid>
+    )
   }
 }
 
-const years_list = gql`{ 
-  arms_year
- }`;
-
-export default graphql(years_list)(Layout);
