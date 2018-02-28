754ee88b1aa0eaefc86e0285c32af9f1a0f20153
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-02-28 00:16:05 -0800

    update query with sidebar filter’s values

--- src/ApolloComponent/chartsQuery.js
+++ src/ApolloComponent/chartsQuery.js
@@ -2,17 +2,18 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export default graphql(gql`
-  query ChartsQuery($years: [Int]) {
+  query ChartsQuery($years: [Int], $report_num: [Int], $subject_num: [Int], $series: [String], $series_element: [Int]) {
     arms_surveydata(
       year: $years,
-      subject_num: 1,
-      series: "farm",
-      series_element: 0,
+      report_num: $report_num,      
+      subject_num: $subject_num,
+      series: $series,
+      series_element: $series_element,
       series2: "farm",
-      series2_element: 0,
-      report_num: [2],
+      series2_element: 0,      
       state_id: "00",
-      topic_abb: ["igcfi", "etot", "evtot", "infi"]
+      topic_abb: ["igcfi", "etot", "evtot", "infi"],
+      order_by: "topic_dim.seq|asc"
     )
     {
       year
@@ -24,9 +25,8 @@ export default graphql(gql`
         seq
         abb
         header
-      }
-      
-    }
+      }      
+    }    
   }
 `, {
     name: 'charts',
--- src/ApolloComponent/initialQuery.js
+++ src/ApolloComponent/initialQuery.js
@@ -8,3 +8,42 @@ export default graphql(gql`
 `, {
     name: 'initial',
   });
+
+//   export default graphql(gql`
+//   query InitialQuery {
+//     arms_year
+//     arms_report(
+//       survey_abb: "finance"
+//     ) {
+//       num
+//       header
+//     }
+//     arms_serie(
+//       abb: ["farm", "grp", "sal", "ftypll", "age", "reg", "spec"]
+//     ){
+//       abb
+//       header
+//       seq
+//       element_dim{
+//         id
+//         invalid
+//         name
+//         serie_abb
+//       }
+//     }
+//     arms_subject (
+//       survey_abb: "finance"
+//     ){
+//       num
+//       header
+//       invalid
+//     }
+//     arms_state{
+//       id
+//       code
+//       name
+//     }
+//   }
+// `, {
+//     name: 'initial',
+//   });
