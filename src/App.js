d3e4daa58af8b81c25180684352935e804efd6f4
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-02-28 00:16:05 -0800

    update query with sidebar filter’s values

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import initial from './ApolloComponent/initialQuery'
 
 class App extends Component {
   render() {
-    if(this.props.initial.arms_year) {
+    if (!this.props.initial.loading) {
       return (
+        // <Layout
+        //  years = {this.props.initial.arms_year}
+        //  states = {this.props.initial.arms_state}
+        //  reports = {this.props.initial.arms_report}
+        //  series = {this.props.initial.arms_serie}
+        //  subjects = {this.props.initial.arms_subject}
+        // />
         <Layout
          years = {this.props.initial.arms_year}
         />
