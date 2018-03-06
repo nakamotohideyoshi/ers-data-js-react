b393fd6f4e9dee33c16f91f85a09629627ea4122
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-06 05:07:03 -0800

    implement state filter

--- src/ApolloComponent/chartsQuery.js
+++ src/ApolloComponent/chartsQuery.js
@@ -2,17 +2,16 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export default graphql(gql`
-  query ChartsQuery($years: [Int], $report_num: [Int], $subject_num: [Int], $series: [String], $series_element: [Int], $series2: [String], $series_element2: [Int], $topic_abb: [String]) {
+  query ChartsQuery($year: [Int], $report_num: [Int], $subject_num: [Int], $series: [String], $series_element: [Int], $series2: [String], $series_element2: [Int], $stats: [String]) {
     arms_surveydata(
-      year: $years,
+      year: $year,
       report_num: $report_num,      
       subject_num: $subject_num,
       series: $series,
       series_element: $series_element,
       series2: $series2,
-      series2_element: $series_element2,
-      topic_abb: $topic_abb,      
-      state_id: "00",
+      series2_element: $series_element2,   
+      state_id: $stats,
       order_by: "topic_dim.seq|asc"
     )
     {
