0754b2489c6364ddc95ff8509ba7cac000489d88
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-06 05:04:55 -0800

    add initial query.

--- src/ApolloComponent/initialQuery.js
+++ src/ApolloComponent/initialQuery.js
@@ -1,49 +1,38 @@
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-export default graphql(gql`
+  export default graphql(gql`
   query InitialQuery {
     arms_year
+    arms_state{
+      id
+      code
+      name
+    }
+    arms_report(
+      survey_abb: "finance"
+    ){
+      num
+      header
+    }
+    arms_subject(
+      survey_abb: "finance"
+    ){
+      num
+      header
+    }
+    arms_serie(
+      abb: ["farm", "grp", "sal", "ftypll", "age", "reg", "n5reg", "spec"]
+    ){
+      abb
+      header
+      element_dim {
+        id
+        name
+        serie_abb
+      }
+    }    
   }
 `, {
     name: 'initial',
   });
-
-//   export default graphql(gql`
-//   query InitialQuery {
-//     arms_year
-//     arms_report(
-//       survey_abb: "finance"
-//     ) {
-//       num
-//       header
-//     }
-//     arms_serie(
-//       abb: ["farm", "grp", "sal", "ftypll", "age", "reg", "spec"]
-//     ){
-//       abb
-//       header
-//       seq
-//       element_dim{
-//         id
-//         invalid
-//         name
-//         serie_abb
-//       }
-//     }
-//     arms_subject (
-//       survey_abb: "finance"
-//     ){
-//       num
-//       header
-//       invalid
-//     }
-//     arms_state{
-//       id
-//       code
-//       name
-//     }
-//   }
-// `, {
-//     name: 'initial',
-//   });
