87be5ed0dd47476739f370c7b339f1e756995da5
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-06 02:39:44 -0600

    Tidy up Chart components

--- src/components/SheetDataChart/index.js
+++ src/components/SheetDataChart/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './style.css'
-import CharView from './generate-chart'
+import ChartGenerator from '../ChartGenerator'
 
 class SheetDataChart extends Component {
   state = {
     incomeArr: []
   }
   componentWillReceiveProps(props) {
-    const { showList, years, surveyData } = props
+    const { showList, surveyData } = props
     let incomeArr = []
-    const c = years.length
     if (surveyData) {
       surveyData.forEach((element, index) => {
           let singleIncome = {}
@@ -48,11 +48,22 @@ class SheetDataChart extends Component {
     else
       return (
         <div className="app">
-          <CharView series={incomeArr} categories={years} title="Farm Business Balance Sheet Data" />
+          <ChartGenerator series={incomeArr} categories={years} title="Farm Business Balance Sheet Data" />
         </div>
       );
   }
 }
 
-// export default withHighcharts(SheetDataChart, Highcharts);
+SheetDataChart.propTypes = {
+  showList: PropTypes.object,
+  surveyData: PropTypes.array,
+  years: PropTypes.array
+};
+
+SheetDataChart.defaultProps = {
+  showList: {},
+  surveyData: [],
+  years: [2014, 2015]
+};
+
 export default SheetDataChart;
