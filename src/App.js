a317b3d619a4f2d9b0ee014d0efebb517b5e4dea
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-07 10:30:28 -0800

    update initialQuery with `arms_filter` query

--- src/App.js
+++ src/App.js
@@ -8,22 +8,32 @@ class App extends Component {
     states: [],
     reports: [],
     subjects: [],
-    series: []
+    series: [],
+    series_element: [],
+    series2: [],
+    series2_element: [],
+    topics: []
+
   }
   
   componentWillReceiveProps(props) {
-    let {years, states, reports, series, subjects} = this.state
+    console.log(props)
+    let {years, states, reports, subjects, series, series_element, series2, series2_element, topics} = this.state
     if (!props.initial.loading) {
-      years = props.initial.arms_year
-      states = props.initial.arms_state
-      reports = props.initial.arms_report
-      subjects = props.initial.arms_subject
-      series = props.initial.arms_serie
+      years = props.initial.arms_filter.year
+      states = props.initial.arms_filter.state
+      reports = props.initial.arms_filter.report
+      subjects = props.initial.arms_filter.subject
+      series = props.initial.arms_filter.serie
+      series2 = props.initial.arms_filter.series2
+      series_element = props.initial.arms_filter.serie_element
+      series2_element = props.initial.arms_filter.serie2_element
+      topics = props.initial.arms_filter.topic
     }
-    this.setState({years, states, reports, subjects, series})
+    this.setState({years, states, reports, subjects, series, series_element, series2, series2_element, topics})
   }
   render() {
-    const {years, states, reports, subjects, series} = this.state
+    const {years, states, reports, subjects, series, series_element, series2, series2_element, topics} = this.state
       return (
         <Layout
          years = {years}
@@ -31,6 +41,10 @@ class App extends Component {
          reports = {reports}
          subjects = {subjects}
          series = {series}
+         series_element = {series_element}
+         series2 = {series2}
+         series2_element = {series2_element}
+         topic = {topics}
         />
       );
   }
