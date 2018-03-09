a177e3fe2da5d803fb6232b9d11fe7e3b1003420
Mario <mariogal.jr@gmail.com>
2018-03-09 12:53:04 -0600

    Checkbox Styling

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './style.css'
 
 const Checkbox = ({ title, checked, arrayIndex, onCheck }) => (
-  <div className="checkbox-2 checkbox-primary">
-    <input type="checkbox" checked={checked} onChange={onCheck} />
-    <label>
+  <div className="checkboxlist-item">
+    <label class="checkbox-container">
       {title}
+      <input type="checkbox" checked={checked} onChange={onCheck} />
+      <span class="checkmark"></span>
     </label>
   </div>
 );
