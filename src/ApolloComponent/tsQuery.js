dabd9903e58c6e8ccbc219b43d777b85b4ade20b
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-27 08:23:49 -0700

    fix type issue in query

--- src/ApolloComponent/tsQuery.js
+++ src/ApolloComponent/tsQuery.js
@@ -8,7 +8,7 @@ import gql from 'graphql-tag';
     $selectedStates: [String],
     $serie: [String]
   ){
-    query23: arms_filter(
+    arms_filter(
       survey_abb: "finance",
       report_num: $report_num,
       subject_num: $subject_num,
