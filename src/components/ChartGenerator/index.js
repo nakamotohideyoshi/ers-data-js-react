0dbf172f5bda9ef23f7cd87e9f8fe9e0ab2180cf
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-27 12:40:12 -0500

    EIWDD 22, 23 - Farms line / Chart X label missing

--- src/components/ChartGenerator/index.js
+++ src/components/ChartGenerator/index.js
@@ -19,11 +19,13 @@ export default class ChartGenerator extends React.Component {
     const { series, categories, title, chartType } = props
     this.generateConfig(series, categories, title, chartType)
   }
-
   generateConfig(series, categories, title, chartType) {
     if (title === '' && series) {
       title = series[0].report
     }
+    const seriesFarms = series.filter(single => single.header === 'Farms')
+    const seriesOthers = series.filter(single => single.header !== 'Farms')
+
     const config = {
       title: {
         text: title
@@ -35,44 +37,57 @@ export default class ChartGenerator extends React.Component {
         type: chartType
       },
       xAxis: {
-        labels: {
-          formatter: function() {
-              return categories[this.value];
+        // labels: {
+        //     formatter: function() {
+        //       return categories[this.value];
+        //     }
+        // },
+        categories: categories,
+        crosshair: true
+      },
+      yAxis: [
+        {
+          title: {
+            text: '$Millions',
+            align: 'high',
+            offset: 0,
+            rotation: 0,
+            y: -20
+          },
+          labels: {
+            formatter: function () {
+                return this.value / 1000000 +'M';
+            }
           }
-        }
-      },
-      yAxis: {
-        title: {
-          text: '$Millions',
-          align: 'high',
-          offset: 0,
-          rotation: 0,
-          y: -20
-        }
-      },
+        },
+      ],
       legend: {
         symbolRadius: 0
       },
       tooltip: {
         useHTML: true,
         formatter: function () {
-            let s = '<span style="font-size:14px; padding:5px;">'+categories[this.x]+'</span><br />'
+            let s = '<span style="font-size:14px; padding:5px;">'+ this.x +'</span><br />'
             s += '<div style="display: flex; flex-direction: column;  max-height: 350px; flex-wrap: no-wrap; margin-top: 5px;"><tr><th /><th /></tr>'
             this.points.forEach((point, index) => {
               s += '<div style="display: flex; justify-content: space-between"><div style="color:'+point.color+'; padding:2px 10px 2px 5px;">'+point.series.name+': </div>' +
               '<div><b>'+(point.y/1000000).toFixed(1)+' million</b></div></div>'
             });
             s += '</div>'
-            console.log(s)
             return s;
         },
         shared: true
       },
       series: []
     }
-    series.forEach((element) => {
+     
+    seriesFarms.forEach((element) => {
+      config.series.push({ data: element.estimateList, name: element.header, visible: element.shown, showInLegend: element.shown, type: 'spline' })
+    })
+    seriesOthers.forEach((element) => {
       config.series.push({ data: element.estimateList, name: element.header, visible: element.shown, showInLegend: element.shown })
     })
+    
     this.setState({ config: Object.assign({}, config) })
   }
   render() {
