9ad4768a273ccb89ea52bb898ff3c461b8d127fd
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-28 05:55:08 -0700

    add new Queries

---
+++ src/ApolloComponent/yAnalysis.js
@@ -0,0 +1,21 @@
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+  export default graphql(gql`
+  query yAnalysis (
+    $selectedYears: [Int]
+  ){
+    arms_filter(
+      survey_abb: "finance",
+      year: $selectedYears
+    ){
+      state{
+        id
+        name
+      }
+    }     
+  }
+`, {
+    skip: (ownProps) => ownProps.runQuery !== 'yAnalysis',
+    name: 'yAnalysis',
+  });
