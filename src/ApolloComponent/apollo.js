f97b277c497626e71f10cb590a97d144caadb051
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-04-02 20:25:16 -0700

    cache is disabled

--- src/ApolloComponent/apollo.js
+++ src/ApolloComponent/apollo.js
@@ -2,6 +2,20 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'cache-and-network',
+    errorPolicy: 'all',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+  mutate: {
+    errorPolicy: 'all',
+  },
+};
+
 const cache = new InMemoryCache({
   dataIdFromObject: object => object.key || null
 })
@@ -9,6 +23,7 @@ const cache = new InMemoryCache({
 const client = new ApolloClient({
   link: createHttpLink({ uri: 'http://query.erspilot.net/graphql?dev=twollegPBxv1pV82fws9'}),
   cache: cache,
+  defaultOptions,
 });
 
 export default client;
