5dfa3845f66e51786534b65532c5d9cb8c66c819
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-04-07 12:30:51 -0700

    update Query

--- src/ApolloComponent/dlfseAnalysis.js
+++ src/ApolloComponent/dlfseAnalysis.js
@@ -3,19 +3,19 @@ import gql from 'graphql-tag';
 
   export default graphql(gql`
   query dlfseAnalysis (
-    $analysis.report_num: [Int],
-    $analysis.topic_abb: [String],
-    $analysis.subject_num: [Int],
-    $analysis.serie: [String],
-    $analysis.serie_element: [Int]
+    $report_num: [Int],
+    $topic_abb: [String],
+    $subject_num: [Int],
+    $serie: [String],
+    $serie_element: [Int]
   ){
     dlfseAnalysis: arms_filter(
       survey_abb: "finance",
-      report_num: $analysis.report_num,
-      topic_abb: $analysis.topic_abb,
-      subject_num: $analysis.subject_num,
-      serie: $analysis.serie,
-      serie_element: $analysis.serie_element
+      report_num: $report_num,
+      topic_abb: $topic_abb,
+      subject_num: $subject_num,
+      serie: $serie,
+      serie_element: $serie_element
     ){
       serie2{
         abb
