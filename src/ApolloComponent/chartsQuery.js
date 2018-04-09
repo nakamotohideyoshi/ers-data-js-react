1087ee95e642e99f1cd68e09495d18e3b8723c60
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-04-09 05:50:45 -0700

    Query update

--- src/ApolloComponent/chartsQuery.js
+++ src/ApolloComponent/chartsQuery.js
@@ -2,17 +2,17 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export default graphql(gql`
-  query ChartsQuery($selectedYears: [Int], $report_num: [Int], $subject_num: [Int], $serie: [String], $serie_element: [Int], $serie2: [String], $serie2_element: [Int], $selectedStates: [String], $topic_abb: [String]) {
+  query ChartsQuery($selectedYears: [Int], $report_num_0: [Int], $subject_num_0: [Int], $serie_0: [String], $serie_element_0: [Int], $serie2_0: [String], $serie2_element_0: [Int], $selectedStates: [String], $topic_abb_0: [String]) {
     arms_surveydata(
       year: $selectedYears,
-      report_num: $report_num,      
-      subject_num: $subject_num,
-      series: $serie,
-      series_element: $serie_element,
-      series2: $serie2,
-      series2_element: $serie2_element,   
       state_id: $selectedStates,
-      topic_abb: $topic_abb,
+      report_num: $report_num_0,      
+      subject_num: $subject_num_0,
+      series: $serie_0,
+      series_element: $serie_element_0,
+      series2: $serie2_0,
+      series2_element: $serie2_element_0, 
+      topic_abb: $topic_abb_0,
       order_by: "topic_dim.seq|asc"
     )
     {
@@ -41,6 +41,7 @@ export default graphql(gql`
     }    
   }
 `, {
+    skip: (ownProps) => ownProps.blockIndex !== 0,
     name: 'charts',
   });
 
