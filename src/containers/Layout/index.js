157eef77e38a94ac69ce41bb04b3c73e844bdd8c
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-04-22 01:01:59 -0700

    EIWDD-62: lock charts and table while change filters

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1714,7 +1714,6 @@ export default class Layout extends React.Component {
     let serie_element = []
     let serie2 = []
     let serie2_element = []
-    isGetSurveyData = false;
 
     for (let i=0; i<9; i++) {
       if (isGetSurveyData) {
