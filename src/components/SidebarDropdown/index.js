2b47e7509586e7c59a61933a9aa86a25d3036f09
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-04-21 21:13:09 -0500

    EIWDD 75 - Reduce height of LHS Sidebar items

--- src/components/SidebarDropdown/index.js
+++ src/components/SidebarDropdown/index.js
@@ -4,15 +4,17 @@ import PropTypes from 'prop-types';
 const SidebarDropdown = ({ title, headingTitle, isCategory, isOpened, onToggle }) => (
   <div className="dropdown_up dropdown_dd">
     <button className={`${isCategory ? `btn-dark`:`btn-light`} btn_dd`} onClick={onToggle} >
-      {
-        headingTitle.length > 0 && (
-          <div>
-            <span className="top_heading">{headingTitle}</span>
-            <br />
-          </div>
-        )
-      }
-      {title}
+      <div>
+        {
+          headingTitle.length > 0 && (
+            <div>
+              <span className="top_heading">{headingTitle}</span>
+              <br />
+            </div>
+          )
+        }
+        {title}
+      </div>
       {
         isOpened && (
           <span className="caret_down">
