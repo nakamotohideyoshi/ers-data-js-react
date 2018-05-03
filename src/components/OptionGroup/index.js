fe341409fc74731a59949f56b8200b8d18604bd8
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-03 20:20:26 +0800

    OptionGroup Font

--- src/components/OptionGroup/index.js
+++ src/components/OptionGroup/index.js
@@ -9,13 +9,16 @@ export default class OptionGroup extends React.Component {
     }
   }
   render() {
-    const { options, selectedIndex, onSelect, tabIndex } = this.props
+    const { options, selectedIndex, onSelect, tabIndex, fontSizeIndex } = this.props
     return (
       <div className="option-group">
       {
         options && (
           options.map((option, index) => {
-            const fontSize = option.size ? { fontSize: option.size } : null
+            let defaultFontSize = '1em'
+            if (fontSizeIndex === 1) defaultFontSize = '1.5em'
+            else if (fontSizeIndex === 2) defaultFontSize = '2em'
+            const fontSize =  { fontSize: option.size ? option.size : defaultFontSize }
             if (index === selectedIndex) {
               return (
                 <div key={index.toString()} className="single-option center-aligned active" onClick={() => onSelect(index)} onKeyDown={event => this.onEnterKeyDown(event, index)} tabIndex={tabIndex+index}>
