0706b42d3161d8605a7c88b1de7281ad37769b7a
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-04 10:27:39 -0700

    Government:  rename query in tailored report

--- src/ApolloComponent/dlfysTailored.js
+++ src/ApolloComponent/dlfysTailored.js
@@ -2,13 +2,13 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
   export default graphql(gql`
-  query ysQuery (
+  query dlfysTailored (
     $report_num: [Int],
     $subject_num: [Int],
     $selectedYears: [Int],
     $serie: [String]
   ){
-    ysQuery: arms_filter(
+    dlfysTailored: arms_filter(
       survey_abb: "finance",
       report_num: $report_num,
       subject_num: $subject_num,
@@ -26,6 +26,6 @@ import gql from 'graphql-tag';
     }     
   }
 `, {
-    skip: (ownProps) => ownProps.runQuery !== 'ysQuery',
-    name: 'ysQuery',
+    skip: (ownProps) => ownProps.runQuery !== 'dlfysTailored',
+    name: 'dlfysTailored',
   });
