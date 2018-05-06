cc9d929c491cb74c55b2d85132daa06f776fdaf2
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-06 00:44:42 -0700

    Government:  add new query and update sidebar component

---
+++ src/ApolloComponent/dlrfTailored.js
@@ -0,0 +1,30 @@
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+  export default graphql(gql`
+  query dlrfTailored (
+    $report_num: [Int],
+    $sub_report: [Int],
+    $subject_num: [Int]
+  ){
+    dlrfTailored: arms_filter(
+      report_num: $report_num
+      sub_report: $sub_report
+      subject_num: $subject_num
+    ){
+      year
+      state {
+        id
+        name
+      }
+    	serie{
+        abb
+        header
+      }
+    }
+     
+  }
+`, {
+    skip: (ownProps) => ownProps.runQuery !== 'dlrfTailored',
+    name: 'dlrfTailored',
+  });
