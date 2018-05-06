31bc8d0424011499bdff6e0053ea344e7fd50bc8
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-06 02:35:19 -0700

    Government:  add new query

---
+++ src/ApolloComponent/dlrftyTailored.js
@@ -0,0 +1,29 @@
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+  export default graphql(gql`
+  query dlrftyTailored (
+    $report_num: [Int],
+    $sub_report: [Int],
+    $subject_num: [Int],
+    $selectedStates: [String],
+    $selectedYears: [Int]
+  ){
+    dlrftyTailored: arms_filter(
+      survey_abb: "finance",
+      report_num: $report_num,
+      sub_report: $sub_report,
+      subject_num: $subject_num,
+      state_id: $selectedStates,
+      year: $selectedYears
+    ){
+      serie{
+        abb
+        header
+      }
+    }     
+  }
+`, {
+    skip: (ownProps) => ownProps.runQuery !== 'dlrftyTailored',
+    name: 'dlrftyTailored',
+  });
