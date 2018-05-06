d2f3b967f295ec733941a72fbe2c734815e21ffd
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-06 02:35:19 -0700

    Government:  add new query

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1233,7 +1233,7 @@ export default class Layout extends React.Component {
         runQuery = pre_filters[blockIndex].report_num[0] === 6 ? 'dlrfyTailored' : 'dlfyTailored'
       } else if (priority.indexOf('year') === 1 &&  priority.indexOf('state') === 0) {
         // State -> Year -> ...
-        runQuery = 'dlftyTailored'
+        runQuery = pre_filters[blockIndex].report_num[0] === 6 ? 'dlrftyTailored' : 'dlftyTailored'
       } else if (priority.indexOf('year') === 1 &&  priority.indexOf('serie') === 0) {
         // Serie -> Year -> ...
         runQuery = 'dlfseyTailored'
@@ -1315,7 +1315,7 @@ export default class Layout extends React.Component {
         runQuery = pre_filters[blockIndex].report_num[0] === 6 ? 'dlrftTailored' : 'dlftTailored'
       } else if (priority.indexOf('state') === 1 &&  priority.indexOf('year') === 0) {
         // Year -> State -> ...
-        runQuery = 'dlftyTailored'
+        runQuery = pre_filters[blockIndex].report_num[0] === 6 ? 'dlrftyTailored' : 'dlftyTailored'
       } else if (priority.indexOf('state') === 1 &&  priority.indexOf('serie') === 0) {
         // Serie -> State -> ...
         runQuery = 'dlfsetTailored'
