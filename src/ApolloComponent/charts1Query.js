f966a5fe0f16b23ddd8196488b56ec1ccaaf52f7
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-07 05:06:23 -0700

    Government:  add new query

---
+++ src/ApolloComponent/charts1Query.js
@@ -0,0 +1,61 @@
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+export default graphql(gql`
+  query Charts1Query($selectedYears: [Int], $sub_report_0: [Int], $report_num_0: [Int], $subject_num_0: [Int], $serie_0: [String], $serie_element_0: [Int], $serie2_0: [String], $serie2_element_0: [Int], $selectedStates: [String], ) {
+    arms_surveydata(
+      year: $selectedYears,
+      state_id: $selectedStates,
+      report_num: $report_num_0,
+      sub_report: $sub_report_0,      
+      subject_num: $subject_num_0,
+      series: $serie_0,
+      series_element: $serie_element_0,
+      series2: $serie2_0,
+      series2_element: $serie2_element_0, 
+      order_by: "topic_dim.seq|asc"
+    )
+    {
+      year
+      rse
+      median
+      estimate
+      unreliable_est
+      report_num
+      topic_abb
+      report_dim{
+        header
+      }
+      subject_dim{
+        header
+      }
+      serie_dim {
+        header
+      }
+      serie_element_dim{
+        name
+      }
+      state{
+        id
+        code
+        name
+      }
+      topic_dim {
+        level
+        seq
+        abb
+        header
+        group_header
+        desc
+        unit_num
+        unit_desc
+      }      
+    }    
+  }
+`, {
+    skip: (ownProps) => !(ownProps.blockIndex === 0 && ownProps.isGetSurveyData && ownProps.report_num_0[0] === 6),
+    name: 'charts1',
+  });
+
+
+  
