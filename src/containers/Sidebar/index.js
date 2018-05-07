d3ca9538827abdbfc12e96751c99ca8078b5d806
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-07 09:33:39 -0700

    Government:  integrated Government payment filter

--- src/containers/Sidebar/index.js
+++ src/containers/Sidebar/index.js
@@ -98,7 +98,7 @@ class Sidebar extends React.Component {
 
       let {categoryTitles, sidebarItems, currentBlock} = this.state
       const runQuery = props.runQuery
-      console.log('props:', runQuery, this.state)
+      console.log('props:', runQuery, props[runQuery])
       if (['initialize'].indexOf(props.runQuery)>-1) {
 
         const reports = this.generateReports(props.reports, currentBlock)
