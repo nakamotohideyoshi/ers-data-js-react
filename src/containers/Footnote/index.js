d00b0d6b94b1a500881741c427775e1455397734
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-23 05:57:04 -0700

    update footnote component

--- src/containers/Footnote/index.js
+++ src/containers/Footnote/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import './style.css';
-import tailorfootnote from '../../ApolloComponent/tailorFootNote'
-import armsdatafootnote from '../../ApolloComponent/armsdataFootNote'
-import { compose } from 'react-apollo'
 
 class Footnote extends React.Component{
   state = {
@@ -12,34 +9,50 @@ class Footnote extends React.Component{
 
   componentWillReceiveProps(props){
     let footnotes = []
-    if (props.blockIndex === 0) {
-      if (props.tailorfootnote) {
-        if(props.tailorfootnote.networkStatus === 7 && props.tailorfootnote.tailorfootnote) {
-          props.tailorfootnote.tailorfootnote.forEach(footnote => {
-            footnotes.push(footnote)
-          })
-        }
-      }
-      this.setState({footnotes})
-    } else {
-      if (props.armsdatafootnote.networkStatus === 7) {
+    if (props.footnotes.length > 0) { 
+      if (props.blockIndex === 0) {      
+        footnotes = props.footnotes 
+        this.setState({footnotes})
+      } else {        
         for (let i=1; i<9; i++) {
-          const datasource = 'datasource'+i
-          if (props.armsdatafootnote[datasource]) {
-            props.armsdatafootnote[datasource].forEach(footnote => {
-              footnotes.push(footnote)
+          const report = 'report_num_' + i
+          const topic = 'topic_abb_' + i
+          if (props[report].length > 0) {
+            props.footnotes.forEach(footnote => {
+              if(footnote.is_global === 0 && footnote.report_num === props[report][0] && props[topic].indexOf(footnote.topic_abb) > -1) {
+                const obj = {}
+                obj.report_num = footnote.report_num
+                obj.topic_abb = footnote.topic_abb
+                obj.text = footnote.text
+                obj.sign = footnote.sign
+                obj.is_global = footnote.is_global
+                obj.datasource = i
+                footnotes.push(obj)
+              } 
             })
           }
         }
-        this.setState({footnotes})
-      }        
-    }    
+
+        props.footnotes.forEach(footnote => {
+          if(footnote.is_global === 1) {
+            const obj = {}
+            obj.report_num = footnote.report_num
+            obj.topic_abb = footnote.topic_abb
+            obj.text = footnote.text
+            obj.sign = footnote.sign
+            obj.is_global = footnote.is_global
+            footnotes.push(obj)
+          } 
+        })
+        this.setState({footnotes})            
+      }
+    }   
   }
 
   render() {
     const { footnotes } = this.state
-    const { fontSizeIndex } = this.props
-
+    const { fontSizeIndex, blockIndex } = this.props
+    // console.log('foot', footnotes)
     return (
       <Col xs={12} md={12} sm={12}>
         <div className={`footnote font-${fontSizeIndex}-normal`}>
@@ -51,17 +64,41 @@ class Footnote extends React.Component{
           }
           <br />
           {
-            footnotes.map((val, index) => {
-              if (val.is_global === 1 || (val.is_global === 0 && val.report_num === this.props.report_num_0[0]))
-                return(
-                  (
-                    <div key={index.toString()}>
-                      <strong>{val.sign}</strong> {' - ' + val.text}<br />
-                    </div>
-                  )                
-                )
-              return null
-            })
+            blockIndex === 0 && (
+              footnotes.map((val, index) => {
+                if (val.is_global === 1 || (val.is_global === 0 && val.report_num === this.props.report_num_0[0]))
+                  return(
+                    (
+                      <div key={index.toString()}>
+                        <strong>{val.sign}</strong> {' - ' + val.text}<br />
+                      </div>
+                    )                
+                  )
+                return null
+              })
+            )
+          }
+          {
+            blockIndex !== 0 && (              
+              footnotes.map((val, index) => {
+                if (val.is_global === 0)
+                  return(
+                    (
+                      <div key={index.toString()}>
+                        <strong>{'DataS Source ' + val.datasource + ': (' + val.sign+')'}</strong> {' - ' + val.text}<br />
+                      </div>
+                    )                
+                  )
+                else 
+                  return(
+                    (
+                      <div key={index.toString()}>
+                        <strong>{val.sign}</strong> {' - ' + val.text}<br />
+                      </div>
+                    )                
+                  )
+              })
+            )
           }
         </div>
       </Col>
@@ -69,8 +106,5 @@ class Footnote extends React.Component{
   }  
 }
 
-export default compose(
-  tailorfootnote,
-  armsdatafootnote,
-)(Footnote);
+export default Footnote;
 
