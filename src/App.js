82714250ae24731570c2d14361cae14212500cd0
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-23 05:54:03 -0700

    move footnote query to higher component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import initial from './ApolloComponent/initialQuery'
 
 class App extends Component {
   state = {
-    reports: []
+    reports: [],
+    footnotes: []
   }
   
   componentWillReceiveProps(props) {
-    let {reports} = this.state
+    let {reports, footnotes} = this.state
     if (props.initial.networkStatus ===7 && props.initial.initial) {
-      reports = props.initial.initial.report      
+      reports = props.initial.initial.report
+      footnotes = props.initial.footnotes    
     }
-    this.setState({reports})
+    this.setState({reports, footnotes})
   }
   render() {
-    const {reports} = this.state
+    const {reports, footnotes} = this.state
       return (
         <Layout
          reports = {reports}
+         footnotes = {footnotes}
         />
       );
   }
